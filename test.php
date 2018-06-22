666c33bfb69c452acd665c15180dd32a29f17c68
SergeyVityazev <37889567+SergeyVityazev@users.noreply.github.com>
2018-06-22 18:22:37 +1000

    Update test.php

--- test.php
+++ test.php
@@ -1,8 +1,22 @@
 <?php
-/*session_start();
-if ($_SESSION['ready']==true)
-    header("Location:Result.php");
-*/
+session_start();
+$RightAnswer=0;
+$NoRightAnswer=0;
+
+foreach ($_POST as $key=>$value){
+    if ($value=='right')
+        $RightAnswer=$RightAnswer+1;
+    if ($value=='Noright')
+        $NoRightAnswer=$NoRightAnswer+1;
+}
+$_SESSION['RightAnswer']=$RightAnswer;
+$_SESSION['NoRightAnswer']=$NoRightAnswer;
+
+if(!file_get_contents("test/".$_SESSION['TestName']))
+    header('HTTP/1.0 404 Not Found');
+if (!empty($_POST))
+    header("Location:Result.php?RightAnswer=$_GET[RightAnswer]&NoRightAnswer=$_GET[NoRightAnswer]&UserName=$_GET[UserName]");
+
 ?>
 
 <!DOCTYPE HTML>
@@ -11,69 +25,49 @@ if ($_SESSION['ready']==true)
 </head>
 <body>
 
+
 <?php
-$NumberTest=$_SESSION['count'];
-echo $_SESSION['NameGuest']." Вы выбрали тест ".$NumberTest;
-$TestText=file_get_contents("test./".$NumberTest);
+
+$NumberTest=$_SESSION['TestName'];
+$TestText=file_get_contents("test/".$NumberTest);
+echo "Здравствуйте! ". $_SESSION['UserName']. " .Вы выбрали тест ".$NumberTest;
 $DecodeTestText=json_decode($TestText,true);
-$stop=false;
-$N=1;
-$M=0;
+
+$right=0;
+$Noright=0;
+$N=0;
 ?>
-<form  method="GET">
+<form  method="post">
     <?php
     foreach ($DecodeTestText as $test) {
         foreach ($test as $inner_key => $questions) {
             if (is_string($questions))
                 continue;
+
             foreach ($questions as $InInner_key => $question) {
                 if ($InInner_key == "content") {
                     echo "<br/>";
                     echo $question ;
                     echo "<br/>";
                 } elseif ($InInner_key !== "nameQ") {
-                    ?>
-                    <label><input type="checkbox" name=<?= (string)$N ?>> <?= $question ?></label>
-                    <?php
-                    $massivAnswer[] = $InInner_key;
-                    $N = $N + 1;
+                    if ($InInner_key=="yes"){
+                        ?> <label><input type="checkbox" name=<?=$right=$right+1 ?> value="right"> <?= $question ?></label>
+                    <?php }
+                    else{ ?>
+                        <label><input type="checkbox" name=<?=$Noright=$Noright+1 ?> value="Noright" > <?= $question ?></label>
+                    <?php  }
                 }
+
+
             }
-        }break;
+        }
     }
+
     ?>
 
     <br/>
     <input type="submit" value="Отправить" />
-
 </form >
-
-
-<?php
-$right=0;
-$Noright=0;
-if (!empty($_GET)) {
-    echo "<br/>";
-    $N = $N - 2;
-    for ($i = 0; $i <= $N; $i++) {
-        if (!empty($_GET[$i+1])) {
-            if ($massivAnswer[$i] == "yes")
-                $right = $right + 1;
-            else
-                $Noright = $Noright + 1;
-        }
-    }
-    echo "<br/>";
-    echo "Ваш результат:";
-    echo "<br/>";
-    echo $right . " Верных ответов";
-    echo "<br/>";
-    echo $Noright ." Неверных ответов";
-    $_SESSION['right']=$right;
-    $_SESSION['Noright']=$Noright;
-    $_SESSION['ready']=true;
-}
-?>
 
 </body>
 </html>
